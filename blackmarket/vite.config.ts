1df533836c9da40eb5022a1676a23f77c6bb750c
Salaam Muiz Bolatito <133219015+zinosalaam1@users.noreply.github.com>
2026-07-20 12:23:02 +0100

    Add build options for Rollup in Vite config

--- blackmarket/vite.config.ts
+++ blackmarket/vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -30,7 +29,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        privacypolicy: path.resolve(__dirname, 'privacypolicy.html'),
+      },
+    },
+  },
 })
